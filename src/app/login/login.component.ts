fdbf219734a8462d1301de149cd00b04f9a60a54
AlyonaAlive <alyonaalive@Helens-MacBook-Air.local>
2020-11-21 09:56:34 +0100

    simple login with firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+// import core firebase client (required)
+import firebase from '@firebase/app';
+// import Firebase Authentication (optional)
+import '@firebase/auth';
+// import Firebase Realtime Database (optional)
+import '@firebase/database';
+// import Cloud Firestore (optional)
+import '@firebase/firestore';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
-  ngOnInit(): void {
+  login(){
+    this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
   }
-
+  
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
+  user: firebase.User; 
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user=>this.user = user)
+   }
 
-  ngOnInit(): void {
+  logout(){
+    this.afAuth.signOut()
   }
 
 }
