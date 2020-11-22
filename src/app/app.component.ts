392e4326882391455fb9af3106bde6abfd71db8a
AlyonaAlive <alyonaalive@Helens-MacBook-Air.local>
2020-11-22 11:03:14 +0100

    save user to db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,10 @@ import { AuthService } from './auth.service';
 export class AppComponent {
   title = 'oshop';
 
-  constructor(private auth: AuthService, private router: Router){
+  constructor(private auth: AuthService, private userService: UserService, private router: Router){
     auth.user$.subscribe(user=>{
       if(user) {
+        userService.save(user);
         let returnUrl = localStorage.getItem('returnUrl');
         router.navigateByUrl(returnUrl)
       }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './auth.service';
 import { AuthGuard as AuthGuard } from './auth-guard.service';
+import { UserService } from './user.service';
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4Eco99lL-bIWwGAr75Y7_hqBdCj7SLEI",
@@ -59,7 +60,8 @@ const firebaseConfig = {
   ],
   providers: [
     AuthService, 
-    AuthGuard
+    AuthGuard,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
