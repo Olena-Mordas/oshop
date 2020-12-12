1af051bf011e392abdb205e79ba5161b35390d8c
AlyonaAlive <alyonaalive@Helens-MacBook-Air.local>
2020-12-12 17:06:31 +0100

    validation & bootstrap card

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
 
@@ -11,7 +12,10 @@ export class ProductFormComponent implements OnInit {
 
   categories$;
   
-  constructor(categoryService: CategoryService, private productService: ProductService) { 
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService, 
+    private productService: ProductService) { 
     this.categories$ = categoryService.getCategories();
   }
 
@@ -21,5 +25,6 @@ export class ProductFormComponent implements OnInit {
   save(product){
     console.log(product);
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { AdminAuthGuard } from './admin-auth-guard.service';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { CategoryService } from './category.service';
 import { ProductService } from './product.service';
+import { CustomFormsModule } from 'ng2-validation';
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4Eco99lL-bIWwGAr75Y7_hqBdCj7SLEI",
@@ -62,7 +63,8 @@ const firebaseConfig = {
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
-    AngularFireStorageModule, NgbModule // storage
+    AngularFireStorageModule, NgbModule ,// storage
+    CustomFormsModule,
 
   ],
   providers: [
