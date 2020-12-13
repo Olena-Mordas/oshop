d88a8d17260ccb33d8915979f66195726fa18dca
AlyonaAlive <alyonaalive@Helens-MacBook-Air.local>
2020-12-13 09:55:43 +0100

    rendering list of products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/product.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit {
 
-  constructor() { }
+  products;
+
+  constructor(private productService: ProductService) {} 
 
   ngOnInit(): void {
+    this.productService.getAll()
+    .subscribe(resp=>this.products=resp);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,4 +11,8 @@ export class ProductService {
   create(product){
     return this.db.list('/products').push(product);
   }
+
+  getAll(){
+    return this.db.list('/products').snapshotChanges();
+  }
 }
