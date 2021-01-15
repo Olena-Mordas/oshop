3711b667a3bd6c0ce8e798c996c699ab15e598ae
Olena Mordas <alyonaalive@gmail.com>
2021-01-15 14:05:00 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AdminAuthGuard } from './admin/services/admin-auth-guard.service';
-import { AdminOrdersComponent } from './admin/components/admin-orders/admin-orders.component';
-import { AdminProductsComponent } from './admin/components/admin-products/admin-products.component';
-import { ProductFormComponent } from './admin/components/product-form/product-form.component';
-import { AuthGuard } from 'shared/services/auth-guard.service';
-import { CheckOutComponent } from './shopping/components/check-out/check-out.component';
-import { HomeComponent } from './core/components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/components/login/login.component';
-import { MyOrdersComponent } from './shopping/components/my-orders/my-orders.component';
-import { OrderSuccessComponent } from './shopping/components/order-success/order-success.component';
 import { ProductsComponent } from './shopping/components/products/products.component';
-import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   {path: '', component: ProductsComponent},
